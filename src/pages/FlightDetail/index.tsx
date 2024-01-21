fbbe0ac0e0b5532e96eba584fb400a3a114b4f1e
Amit Kumar Gouda <amitgouda17995@gmail.com>
2024-01-22 05:22:47 +0530

    - Minor changes

--- src/pages/FlightDetail/index.tsx
+++ src/pages/FlightDetail/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { FLIGHT_DATA } from "../Home/interface";
 import { COLUMNS } from "../Home/constants";
-
+import "./flightDetail.css";
 const FlightDetail: React.FC = () => {
   const { id } = useParams();
   const [flightData, setFlightData] = useState<FLIGHT_DATA>();
@@ -22,14 +22,23 @@ const FlightDetail: React.FC = () => {
   };
 
   return (
-    <div>
-      <div>
-        <span>Flight Details</span>
+    <div className="flight-detail-container">
+      <div className="detail-container">
+        <div className="detail-header">Flight Details</div>
+
         <div>
-          {COLUMNS.map((columnData) => (
-            <div>
-              {" "}
-              {columnData.name} = {flightData && flightData[columnData.key]}{" "}
+          {COLUMNS.map((columnData, index) => (
+            <div
+              className={`detail-row ${index % 2 === 0 ? "detail-row-1" : ""}`}
+            >
+              <div className="detail-column ">{columnData.name} </div>
+              <div className="detail-column">
+                {flightData
+                  ? typeof columnData.helperFunc === "function"
+                    ? columnData.helperFunc(flightData[columnData.key])
+                    : flightData[columnData.key]
+                  : ""}
+              </div>
             </div>
           ))}
         </div>
